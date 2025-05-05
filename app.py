d478fcd9d11086cebe0ab9964bfdc20884a1bbf6
Ayman Ashraf <148762351+emvsworld9@users.noreply.github.com>
2025-05-05 19:24:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Weqaya", layout="wide")
 # Navigation
 PAGES = {
     "Home": r"home_page.py",
-    "Hypertension ": r"first.py",
+    "Hypertension": r"first.py",
     "Storke":r"third.py"
 }
 
@@ -15,7 +15,7 @@ page = st.sidebar.radio("Select a page", list(PAGES.keys()))
 # Display the selected page
 if page == "Home":
     exec(open(PAGES["Home"], encoding="utf-8").read())
-elif page == "Hypertension ":
+elif page == "Hypertension":
     exec(open(PAGES["Hypertension"]).read())
 elif page == "Diabetes":
     exec(open(PAGES["Diabetes"], encoding="utf-8").read())
