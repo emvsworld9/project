becfc7e63e44a89648da6cc4fc176e39949a67c2
Ayman Ashraf <148762351+emvsworld9@users.noreply.github.com>
2025-05-05 20:07:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Weqaya", layout="wide")
 PAGES = {
     "Home": r"home_page.py",
     "Hypertension": r"first.py",
-    "Storke":r"third.py"
+    "Stroke":r"third.py"
 }
 
 st.sidebar.title("Navigation")
