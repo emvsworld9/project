ba415ab862ae0b41d533a7e7fae1170dfe89c092
Ayman Ashraf <148762351+emvsworld9@users.noreply.github.com>
2025-05-07 02:11:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ if page == "Home":
 elif page == "Hypertension":
     exec(open(PAGES["Hypertension"]).read())
 elif page == "Diabetes":
-    exec(open(PAGES["Diabetes"], encoding="utf-8").read())
+    with open(PAGES["Diabetes"], "r", encoding="utf-8") as f:
+    content = f.read()
+
+# Replace bad Unicode characters like non-breaking spaces
+    cleaned_content = content.replace("\u00A0", " ")
+
+# Then run it
+    exec(cleaned_content)
 else:
     exec(open(PAGES["Stroke"], encoding="utf-8").read())
